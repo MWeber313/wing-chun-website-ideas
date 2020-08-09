c88866c9e662fb40b49d147435c0cf55d5e07069
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-08-08 18:06:19 -0700

    Updated starter file

---
+++ wingchunwa/js/react/header.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const footerItem = React.createElement;
+
+class FooterBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+        return (
+        <div>
+            <p>Join us on Facebook</p>
+            <p>Follow us on Twitter</p>
+            <p>Check us out on YouTube</p>
+            <p>
+                Fei Mong Wing Chun &copy; Ed Dumancas <br/>
+                Website built and designed by <a href="#">Mackenzie Weber</a> 
+            </p>
+        </div>
+    )
+
+}
+
+const footerContainer = document.querySelector('react-footer-container')
+ReactDOM.render(footerItem(FooterBar), footerContainer);
