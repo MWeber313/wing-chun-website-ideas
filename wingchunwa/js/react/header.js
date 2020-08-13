45a5165b40771b0381c391c9612174e08445331f
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-08-12 21:16:17 -0700

    Updating links, header react component

--- wingchunwa/js/react/header.js
+++ wingchunwa/js/react/header.js
@@ -11,13 +11,13 @@ class HeaderBar extends React.Component {
         return (
         <div>
             <nav>
-                <a href="../index.html">Home</a>
-                <a href="html/about.html">About Us</a>
-                <a href="html/acot.html">A Cup of Tea</a>
-                <a href="html/videos.html">Videos</a>
-                <a href="html/classes.html">Classes</a>
-                <a href="html/story.html">Sifu's Story</a>
-                <a href="html/contact.html">Contact Us</a>
+            <a href="index.html">Home</a>
+            <a href="about.html">About Us</a>
+            <a href="acot.html">A Cup of Tea</a>
+            <a href="videos.html">Videos</a>
+            <a href="classes.html">Classes</a>
+            <a href="story.html">Sifu's Story</a>
+            <a href="contact.html">Contact Us</a>
             </nav>
         </div>
     )
